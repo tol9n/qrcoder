e079fdc2309849499ccc011bce7973ecc1b2326b
tol9n <Shilkov109@gmail.com>
2024-09-03 14:34:18 +0500

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, send_file
+import qrcode
+app = Flask(__name__)
+
+
+@app.route("/service/qr")
+def qrcoder():
+    address = request.headers.get('payload')
+    qr = qrcode.make(address)
+    qr.save('code.png')
+    return send_file('code.png', mimetype='image/gif')
+
+
+if __name__ == '__main__':
+    app.run(host='172.30.105.99', port=5001)
